d8db2214a9da1bd9837eb40bfc26359f2cdbb39f
Aaron <aaroncleary13@gmail.com>
2020-08-14 01:50:29 +0100

    Create exercise1-3.c

---
+++ exercise1-3.c
@@ -0,0 +1,21 @@
+/* 19/07/2019 */
+
+# include <stdio.h>
+
+int main(){
+
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	printf("\nfahr\tcelsius\n----------------\n");
+	while(fahr <= upper){
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
+		fahr += step;
+	}
+}
